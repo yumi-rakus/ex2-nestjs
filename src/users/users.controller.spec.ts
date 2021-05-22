56028d2546fe1e4cec5838a57d100ed3467d669a
yumi-rakus <yumi.takahashi@rakus-partners.co.jp>
2021-05-22 19:21:01 +0900

    UsersControllerのテストを修正

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,16 +5,37 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { Gender } from './schemas/user.enum';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+const mockService = () => ({
+  readUser: jest.fn(),
+  readAllUsers: jest.fn(),
+  updateUser: jest.fn(),
+  deleteUser: jest.fn(),
+  createUser: jest.fn(),
+});
+
 describe('UserController', () => {
   let usersController: UsersController;
+  let usersService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useFactory: mockService,
+        },
+      ],
     }).compile();
 
-    usersController = app.get<UsersController>(UsersController);
+    usersController = await module.get<UsersController>(UsersController);
+    usersService = await module.get<UsersService>(UsersService);
+  });
+
+  describe('test', () => {
+    it('testtest', () => {
+      expect(true).toBeTruthy();
+    });
   });
 
   describe('addUser', () => {
@@ -24,13 +45,19 @@ describe('UserController', () => {
         gender: Gender.Male,
         age: 22,
       };
-      expect(await usersController.addUser(newUser)).toHaveProperty('id');
+      const mockUserId = 'userId';
+      usersService.createUser.mockResolvedValue(mockUserId);
+      expect(usersService.createUser).not.toHaveBeenCalled();
+
+      const result = await usersController.addUser(newUser);
+      expect(usersService.createUser).toHaveBeenCalledWith(newUser);
+      expect(result.id).toEqual(mockUserId);
     });
   });
 
   describe('getAllUsers', () => {
     it('should return all users', async () => {
-      const users: CreateUserDto[] = [
+      const users: Array<CreateUserDto> = [
         {
           name: 'Tom',
           gender: Gender.Male,
@@ -47,12 +74,15 @@ describe('UserController', () => {
           age: 30,
         },
       ];
+      usersService.readAllUsers.mockResolvedValue(
+        users.map((user, index) => ({ ...user, id: 'userId' + index })),
+      );
 
-      await usersController.addUser(users[0]);
-      await usersController.addUser(users[1]);
-      await usersController.addUser(users[2]);
+      expect(usersService.readAllUsers).not.toHaveBeenCalled();
 
-      expect(await usersController.getAllUsers()).toEqual(
+      const result = await usersController.getAllUsers();
+      expect(usersService.readAllUsers).toHaveBeenCalled();
+      expect(result).toEqual(
         users.map((user) => ({ ...user, id: expect.anything() })),
       );
     });
@@ -60,95 +90,51 @@ describe('UserController', () => {
 
   describe('getUser', () => {
     it('should return a user', async () => {
-      const users: CreateUserDto[] = [
-        {
-          name: 'Tom',
-          gender: Gender.Male,
-          age: 22,
-        },
-        {
-          name: 'Mary',
-          gender: Gender.Female,
-          age: 19,
-        },
-        {
-          name: 'Keid',
-          gender: Gender.Male,
-          age: 30,
-        },
-      ];
+      const mockUser = {
+        id: 'userId',
+        name: 'Tom',
+        gender: Gender.Male,
+        age: 22,
+      };
+      usersService.readUser.mockResolvedValue(mockUser);
+      expect(usersService.readUser).not.toHaveBeenCalled();
 
-      const user0 = await usersController.addUser(users[0]);
-      const user1 = await usersController.addUser(users[1]);
-      const user2 = await usersController.addUser(users[2]);
-
-      const userId0 = user0.id;
-      const userId1 = user1.id;
-      const userId2 = user2.id;
-
-      expect(await usersController.getUser(userId0)).toEqual({
-        ...users[0],
-        id: userId0,
-      });
-      expect(await usersController.getUser(userId1)).toEqual({
-        ...users[1],
-        id: userId1,
-      });
-      expect(await usersController.getUser(userId2)).toEqual({
-        ...users[2],
-        id: userId2,
-      });
+      const mockUserId = 'userId';
+      const result = await usersController.getUser(mockUserId);
+      expect(usersService.readUser).toHaveBeenCalledWith(mockUserId);
+      expect(result).toStrictEqual(mockUser);
     });
   });
 
   describe('updateUser', () => {
     it('should update the user', async () => {
-      const newUser: CreateUserDto = {
-        name: 'Mary',
-        gender: Gender.Female,
-        age: 19,
-      };
-
-      const addedUser = await usersController.addUser(newUser);
-      const addedUserId = addedUser.id;
+      const updateUserId = 'updateUserId';
       const updateUser: UpdateUserDto = {
         name: 'Super Man',
         gender: Gender.Male,
         age: 100,
       };
+      usersService.updateUser.mockResolvedValue(null);
+      expect(usersService.updateUser).not.toHaveBeenCalled();
 
-      await usersController.updateUser(addedUserId, updateUser);
-
-      expect(await usersController.getUser(addedUserId)).toEqual({
-        ...updateUser,
-        id: addedUserId,
-      });
+      const result = await usersController.updateUser(updateUserId, updateUser);
+      expect(usersService.updateUser).toHaveBeenCalledWith(
+        updateUserId,
+        updateUser,
+      );
+      expect(result).toBeNull();
     });
   });
 
   describe('removeUser', () => {
     it('should remove the user', async () => {
-      const users: CreateUserDto[] = [
-        {
-          name: 'Keid',
-          gender: Gender.Male,
-          age: 30,
-        },
-        {
-          name: 'Jobs',
-          gender: Gender.Male,
-          age: 56,
-        },
-      ];
+      const deleteUserId = 'deleteUserId';
+      usersService.deleteUser.mockResolvedValue(null);
+      expect(usersService.deleteUser).not.toHaveBeenCalled();
 
-      await usersController.addUser(users[0]);
-      const user1 = await usersController.addUser(users[1]);
-      const userId1 = user1.id;
-
-      await usersController.removeUser(userId1);
-
-      expect(await usersController.getAllUsers()).toHaveLength(1);
-      expect(async () => await usersController.getUser(userId1)).toThrow();
+      const result = await usersController.removeUser(deleteUserId);
+      expect(usersService.deleteUser).toHaveBeenCalledWith(deleteUserId);
+      expect(result).toBeNull();
     });
   });
 });
